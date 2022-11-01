1ffd448dcc258e5d5b58b456e93600bf0bffd005
IdrisCafarov <idris.ceferov244@gmail.com>
2022-11-01 23:41:08 +0400

    parser

--- settings/settings.py
+++ settings/settings.py
@@ -47,6 +47,14 @@ INSTALLED_APPS = [
     'corsheaders',
 ]
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+    ]
+}
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
