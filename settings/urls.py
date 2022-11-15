786f33901f7a069067b33af2182f1fd328e155d2
IdrisCafarov <idris.ceferov244@gmail.com>
2022-11-15 19:40:40 +0400

    hope last

--- settings/urls.py
+++ settings/urls.py
@@ -17,13 +17,17 @@ from django.contrib import admin
 from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
+import re
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("blog.urls")),
+    re_path(r'^%s(?P<path>.*)$' % re.escape(settings.STATIC_URL.lstrip('/')), serve, {"document_root": settings.STATIC_ROOT}),
 ]
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
