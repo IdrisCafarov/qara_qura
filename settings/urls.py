57fcec5cb17ff4e75fe6378299afe5e757232f7a
IdrisCafarov <idris.ceferov244@gmail.com>
2022-11-24 15:38:46 +0400

    admin path

--- settings/urls.py
+++ settings/urls.py
@@ -20,8 +20,9 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('',include("blog.urls")),
+    path('', admin.site.urls),
+    
 ]
 
 
